b6bdcba77d7fcc40146cc6e88181eb2c0df97f77
mimaraslan <mimaraslan@gmail.com>
2024-10-27 11:01:54 +0300

    Java SE Operatorler
    Java SE Operatorler

---
+++ java-03-operatorler/src/main/java/com/mimaraslan/AppMain.java
@@ -0,0 +1,17 @@
+package com.mimaraslan;
+
+// tek satirlik aciklama
+
+/*
+birden  fazla
+satirda aciklama
+*/
+
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+    }
+}
