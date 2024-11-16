8283a6d05f929048c9bef093c398c1af744c37fb
mimaraslan <mimaraslan@gmail.com>
2024-11-16 16:50:11 +0300

    SQL
    SQL

---
+++ java-22-sql/src/main/resources/_01_Day_CreateCompanyDBMySQL.sql
@@ -0,0 +1,39 @@
+-- VERITABANINI OLUSTURYORUZ
+CREATE SCHEMA `my_company_db` DEFAULT
+    CHARACTER SET utf32 COLLATE utf32_bin ;
+
+
+
+
+
+--  MUSTERILER TABLOSU
+CREATE TABLE `my_company_db`.`customers` (
+                                             `ID` INT NOT NULL AUTO_INCREMENT,
+                                             `FIRST_NAME` VARCHAR(60) NULL,
+                                             `LAST_NAME` VARCHAR(60) NULL,
+                                             PRIMARY KEY (`ID`));
+
+
+
+
+
+-- URUNLER TABLOSU
+CREATE TABLE `my_company_db`.`products` (
+                                            `ID` INT NOT NULL AUTO_INCREMENT,
+                                            `PROD_NAME` VARCHAR(200) NULL,
+                                            `PROD_CODE` VARCHAR(45) NULL,
+                                            PRIMARY KEY (`ID`));
+
+-- TABLO UZERINDE DEGISIKLIK YAPAR
+ALTER TABLE `my_company_db`.`products`
+    CHANGE COLUMN `PROD_Code` `PROD_CODE` VARCHAR(45) NULL DEFAULT NULL ;
+
+-- URUNLER TABLOLSUNU SILER
+-- DROP TABLE `mycompany`.`products`;
+
+-- VERITABINI SILER
+-- DROP DATABASE `my_company_db`;
+
+-- MUSTERILERI LISTELE GOSTER
+SELECT * FROM my_company_db.customers;
+
---
+++ java-22-sql/src/main/resources/_01_Day_CreateCompanyDBPostgreSQL.sql
@@ -0,0 +1,32 @@
+-- VERITABANI DB OLUSTURULDU
+CREATE SCHEMA my_company_db
+    AUTHORIZATION postgres;
+
+
+-- MUSTERILER TABLOSU OLUSTURULDU
+CREATE TABLE my_company_db.customers
+(
+    "ID" serial NOT NULL,
+    "FIRST_NAME" character varying(60),
+    "LAST_NAME" character varying(60),
+    PRIMARY KEY ("ID")
+);
+
+ALTER TABLE IF EXISTS my_company_db.customers
+    OWNER to postgres;
+
+
+-- PRODUCT TABLOSU OLUSTURULDU
+CREATE TABLE my_company_db.products
+(
+    "ID" bigserial NOT NULL,
+    "PROD_NAME" character varying(200),
+    "PROD_CODE" character varying(45),
+    PRIMARY KEY ("ID")
+);
+
+ALTER TABLE IF EXISTS my_company_db.products
+    OWNER to postgres;
+
+-- MUSTERILERI LISTELE GOSTER
+SELECT * FROM my_company_db.customers;
