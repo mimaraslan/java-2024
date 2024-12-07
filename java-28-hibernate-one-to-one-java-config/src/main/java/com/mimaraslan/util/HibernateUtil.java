30e687998d64fbef0f19aa53f390501609f690b7
mimaraslan <mimaraslan@gmail.com>
2024-12-07 12:20:52 +0300

    Hibernate Java Config
    Hibernate Java Config

--- java-28-hibernate-one-to-one-java-config/src/main/java/com/mimaraslan/util/HibernateUtil.java
+++ java-28-hibernate-one-to-one-java-config/src/main/java/com/mimaraslan/util/HibernateUtil.java
@@ -1,31 +1,66 @@
 package com.mimaraslan.util;
 
+import com.mimaraslan.entity.Customer;
+import com.mimaraslan.entity.CustomerDetail;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class HibernateUtil {
 
-    private static ServiceRegistry serviceRegistry;
+    private static SessionFactory sessionFactory;
 
-    static{
+    public static SessionFactory getSessionFactory() {
 
-        try{
-            Configuration configuration=new Configuration();
-            configuration.configure();
+        if (sessionFactory == null) {
 
-            serviceRegistry= new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-            configuration.buildSessionFactory(serviceRegistry);
+            try {
+                Configuration configuration = new Configuration();
 
-        }catch(Exception e){
-            System.err.println("Exception : "+e);
+
+                Properties settings = new Properties();
+
+/*
+                // MYSQL
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/companydb?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "123456789");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+*/
+
+                // PostgreSQL
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/companydb");
+             // settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/postgres?currentSchema=companydb");
+                settings.put(Environment.USER, "postgres");
+                settings.put(Environment.PASS, "123456789");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+
+
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.FORMAT_SQL, "true");
+                settings.put(Environment.HBM2DDL_AUTO, "create");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(Customer.class);
+                configuration.addAnnotatedClass(CustomerDetail.class);
+
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
+        return sessionFactory;
     }
-
-    public static SessionFactory getSessionFactory(){
-        return new Configuration().configure().buildSessionFactory();
-    }
-
 }
