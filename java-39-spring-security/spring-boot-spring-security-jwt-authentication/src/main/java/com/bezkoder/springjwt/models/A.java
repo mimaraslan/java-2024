ecae76fc77196aedce224349b820f9809ba6e77c
Mimar Aslan <mimaraslan@gmail.com>
2025-01-11 17:29:21 +0300

    Spring Security
    Spring Security

---
+++ java-39-spring-security/spring-boot-spring-security-jwt-authentication/src/main/java/com/bezkoder/springjwt/models/A.java
@@ -0,0 +1,39 @@
+package com.bezkoder.springjwt.models;
+
+public class A {
+
+/*
+    @NotNull(message = "Please enter your username")
+    @Size(min = 4, max = 16, message = "Your username should be at least 4 chars")
+    @Pattern(regexp = "\\A(?!\\s*\\Z).+", message = "Your username must consist of the characters .")
+    private String username;
+
+    @NotNull(message = "Please enter your email")
+    @Size(min = 10, max = 80)
+    @Email
+    private String email;
+
+    @NotNull(message = "Please enter your password")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$",
+            message = "{FriendlyMessageCodes__CONSTRAINT_PASSWORD_PATTERN}") //"Your password must consist of the characters a-z, A-Z, 0-9."
+    private String password;
+
+
+    @NotNull(message = "Please enter your phone number")
+    @Size(min = 12, max = 12, message = "Phone number should be exact 12 chars")
+    @Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$",
+            message = "Please enter valid phone number")
+    private String phoneNumber;
+
+    @NotNull
+    @Pattern(regexp = "^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!0000)[0-9]{4}$",
+            message = "Please enter valid SSN number")
+    private String ssn;
+
+    @NotNull(message = "Please enter your birthday")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @Past
+    private LocalDate birthDay;
+*/
+
+}
